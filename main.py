e64d67ecf18ba256f47d982c97d211f93e91ee67
AlexandroProvenzale <alexprovenzale@gmail.com>
2023-02-12 17:10:00 -0600

    Consultas listadas en consola y almacenadas en archivos de texto, también modificación de métodos de ejecución de scripts

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ sqlPaths = {
     'realizarConsultas': rootPath + "RealizarConsultas.sql"
 }
 
+
 def newConnection():
     try:
         connection = sql.connect(
@@ -29,6 +30,7 @@ def newConnection():
         return connection
     except sql.Error as error:
         print(error)
+
 
 def chooseOpt():
     global opt
@@ -42,7 +44,10 @@ def chooseOpt():
             print(f'\nElegiste la opción {opt}')
             break
 
-def exec(conn, path):
+
+def execScript(conn, path, select):
+    global cntr
+    cntr = 0
     file = open(path)
     reader = file.read()
     cursor = conn.cursor()
@@ -50,50 +55,59 @@ def exec(conn, path):
         for statement in reader.split(';'):
             if len(statement) > 2:
                 cursor.execute(statement)
+                if select:
+                    cntr += 1
+                    rows = cursor.fetchall()
+                    if cntr != 10: #Cambiar a 3 por favooooooooooor
+                        string = ""
+                        print("\nConsulta " + str(cntr) + "\n")
+                        header = False
+                        for row in rows:
+                            if not header:
+                                string += ", ".join(t[0] for t in row.cursor_description) + "\n"
+                                print("| ", " | ".join(t[0] for t in row.cursor_description), " |")
+                                header = True
+                            print("| ", " | ".join(str(item) for item in row), " |")
+                            string += ", ".join(str(item) for item in row) + "\n"
+                        f = open("Consulta" + str(cntr) + ".csv", "w")
+                        f.write(string)
+                        f.close()
+                    else:
+                        continue
+        conn.commit()
     except sql.Error as error:
         print("\n" + error)
 
-def execSelect(conn, path):
-    file = open(path)
-    reader = file.read()
-    cursor = conn.cursor()
-    try:
-        for statement in reader.split(';'):
-            if len(statement) > 2:
-                cursor.execute(statement)
-                response = cursor.fetchall()
-                print(response)
-    except sql.Error as error:
-        print("\n" + error)
 
-    
 def main():
     global opt
     conn = newConnection()
     while True:
-        print('''
+        print("""
         1. Borrar modelo
         2. Crear modelo
         3. Extraer informacion
         4. Cargar informacion
         5. Realizar consultas
         6. Salir
-        ''')
+        """
+        )
 
         chooseOpt()
 
         if opt == 1:
-            exec(conn, sqlPaths['borrarModelo'])
+            execScript(conn, sqlPaths['borrarModelo'], False)
+            print("\nSe borró el modelo correctamente")
         elif opt == 2:
-            exec(conn, sqlPaths['crearModelo'])
+            execScript(conn, sqlPaths['crearModelo'], False)
+            print("\nSe creó el modelo correctamente")
         elif opt == 3:
             continue
         elif opt == 4:
-            exec(conn, sqlPaths['cargarInformacion'])
+            execScript(conn, sqlPaths['cargarInformacion'], False)
+            print("\nSe Cargó la información al modelo correctamente")
         elif opt == 5:
-            cursor = execSelect(conn, sqlPaths['realizarConsultas'])
-            response = cursor.fetchall()
-            print(response)
+            execScript(conn, sqlPaths['realizarConsultas'], True)
         elif opt == 6:
             print("\nHasta la vista")
             break
